72083701784bd3e5b43b564074afce49c14a0fe6
Alinawaz <alinawaz@gmail.com>
2026-09-25 02:26:58 -0700

    added haq nawaz button and fixed urls

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,16 @@
 import streamlit as st
-st.title("✅ Synced Todo list")
-st.write("Your list lives in an npoint document.save the sync code to pick it up again any where")
-name=st.text_input("Sync Code")
 
-    
-import streamlit as st
+st.title("✅ Synced Todo list")
+st.write("Your list lives in an npoint document. Save the sync code to pick it up again anywhere.")
+me = st.text_input("Sync Code")
 
 with st.sidebar:
     st.header('server')
-    st.text_input(label='Main app URL', value='http://localhost:3001')
-    st.text_input(label='Main Api URL', value='http://api.localhost:3001')
+    st.text_input(label='Main app URL', value='https://my-first-app-hebza4pl2unseyllaftutk.streamlit.app')
+    st.text_input(label='Main Api URL', value='https://my-first-app-hebza4pl2unseyllaftutk.streamlit.app')
+
 tab_new, tab_existing = st.tabs(["start a new list", "open an existing list"])
+st.button("big boos haq nawaz")
 
 
     
